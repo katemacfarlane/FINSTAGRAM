4fc09b5179a4281033449f71582b5b379b8eb8c5
Kate <kate.macfarlane4@gmail.com>
2016-07-14 02:33:18 +0000

    W4D2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
     has_many :likes
     has_many :comments
     
+    validates_presence_of :email, :avatar_url, :username, :password
+    validates_uniqueness_of :email, :username
+
 end
