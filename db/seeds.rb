1d6b9346a9389af3f388bccaae25d04fbe45554c
Kate <kate.macfarlane4@gmail.com>
2016-09-21 12:26:48 +0000

    RSM459

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 users = [
-    { username: "katemacfarlane", avatar_url: "https://pbs.twimg.com/profile_images/689895732497813504/3MPlb0V7_400x400.jpg" },
-    { username: "alleethompson", avatar_url: "https://pbs.twimg.com/profile_images/489918105238269953/rc8Yhb5V.jpeg" },
-    { username: "georgiagracevogeli", avatar_url: "https://pbs.twimg.com/profile_images/743646889426329601/e-Fpphll_400x400.jpg" }
+    { username: "katemacfarlane", avatar_url: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/7/005/098/074/078049c.jpg" },
+    { username: "profferguson", avatar_url: "http://bridgeable.com/wp-content/uploads/Ferguson-Chris-700x470.jpg" },
+    { username: "profleung", avatar_url: "https://www.rotman.utoronto.ca/-/media/Images/Programs-and-Areas/ExecutiveProgram/EP_Faculty/Mark-Leung.JPG?h=100&la=en&w=100&hash=44FABFE357849DC1978F9BB950E0E9306C9B6D86" }
 ]
 
 users.each do |user|
@@ -10,8 +10,8 @@ end
 
 posts = [
     { photo_url: "https://pbs.twimg.com/media/CfoKws4WEAI28Ff.jpg:large", user_id: User.find_by({username: 'katemacfarlane'}).id},
-    { photo_url: "https://pbs.twimg.com/media/B-BAFadCAAEQW48.jpg", user_id: User.find_by({username: 'alleethompson'}).id },
-    { photo_url: "https://pbs.twimg.com/media/CR72DmEWIAAmUO1.jpg", user_id: User.find_by({username: 'georgiagracevogeli'}).id }
+    { photo_url: "http://bridgeable.com/wp-content/uploads/IMG_5125-crop-1600x540.jpg", user_id: User.find_by({username: 'profferguson'}).id },
+    { photo_url: "http://kr.designmarketinglab.com/wp-content/uploads/sites/2/2013/01/rotman-designworks.jpg", user_id: User.find_by({username: 'profleung'}).id }
 ]
 
 posts.each do |post|
